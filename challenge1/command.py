cec8a93402eeeaea772146501a1e844d19ae1e3f
Marek Ciepiela <xmarkurion@gmail.com>
2023-03-07 12:25:00 +0000

    Round1

--- challenge1/command.py
+++ challenge1/command.py
@@ -1,4 +1,23 @@
 
+print("Enter the name (upper case for the first letter)") 
+inputFour = input(message)
+
+match inputFour:
+    case "Cathal":
+        print("Everybody loves " + inputFour) 
+    case "Oli":
+        print("Everybody loves " + inputFour) 
+    case "Eoghan":
+        print("Everybody loves " + inputFour) 
+    case "Marek":
+        print("Everybody loves " + inputFour) 
+        
+
+    case _:
+        print(inputFour + " is not a valid team member :/")
+
+
+
 def hello(message):
     # When this command is used, everything after the word "hello" in the message will be sent to this function.
     # Example: "@Jam hello Ronan" -> this function receivces "Ronan" as the messsage.
@@ -11,7 +30,6 @@ def hello(message):
         return f"Hello, {message}!"
     else:
         return f"Hello, Cisco!"
-    
 
 # //If the message length is greater than 0,
 #  you can assume someone entered their name. Return a message in the format **“Hello, (name)!”**.
