83d3b2d9ec15297db5b0260a7792e123cffc2ef7
Marek Ciepiela <xmarkurion@gmail.com>
2023-03-07 12:16:14 +0000

    1.2

--- challenge1/command.py
+++ challenge1/command.py
@@ -24,12 +24,5 @@ def hello(message):
     #
     # Your job is to process the message so that this function returns the correct outputs for challenge 1.
     # (for now, it just echoes back the same message)
-    if message == "Chuck Robbins":
-        return f"Hello Cisco's favourite CEO Chuck Robbins"
-    if( len(message) > 0 ):
-        return f"Hello, {message}!"
-    else:
-        return f"Hello, Cisco!"
-
-# //If the message length is greater than 0,
-#  you can assume someone entered their name. Return a message in the format **“Hello, (name)!”**.
+    rValue = "Hello, Cisco!"
+    return rValue
